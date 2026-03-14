11d1b64fa1f6625f141b7b85ded7125e9f87f344
Valentin Brosseau <c4software@gmail.com>
2026-03-14 13:53:45 +0100

    Fix store matching when using sqlite

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -106,10 +106,10 @@ class Store extends Model
         $first = array_shift($domains);
 
         return $query->where(function (Builder $subQuery) use ($first, $domains) {
-            $subQuery->whereJsonContains('domains', ['domain' => $first]);
+            $subQuery->whereLike('domains', '%"domain":"'.$first.'"%');
 
             foreach ($domains as $domain) {
-                $subQuery->orWhereJsonContains('domains', ['domain' => $domain]);
+                $subQuery->orWhereLike('domains', '%"domain":"'.$domain.'"%');
             }
         });
     }
